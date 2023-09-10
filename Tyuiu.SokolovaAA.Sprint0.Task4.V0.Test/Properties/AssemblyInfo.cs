39ef9627375fd52e8bb3436d23afe242b9b193ba
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-09-10 20:31:24 +0500

    Task4

---
+++ Tyuiu.SokolovaAA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SokolovaAA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SokolovaAA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("10a6120d-2430-4bfb-917a-14f285f1e412")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.SokolovaAA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaAA.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.SokolovaAA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //пример линейной структуры программы
+            //вызов метода сложения addition
+            Console.WriteLine(DataService.Addition(1, 5));
+            //вызов метода умножения multiplication
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //вызов метода деления division
+            Console.WriteLine(DataService.Division(5, 5));
+            //вызов вычитания метода вычитания subtraction
+            Console.WriteLine(DataService.Subtraction(15, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
