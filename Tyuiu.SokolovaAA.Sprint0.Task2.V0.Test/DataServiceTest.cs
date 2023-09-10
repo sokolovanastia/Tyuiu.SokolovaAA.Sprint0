5c2a1f3a1add5f6b3c4efd3d386e522e344522e0
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-09-10 18:41:49 +0500

    Task2

---
+++ Tyuiu.SokolovaAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SokolovaAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SokolovaAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            // Область создания методов тестирования, методов из библиотеки
+            var name = "Настя";
+            var res = DataService.GetMessage(name);
+
+            // Вызываем класс Assert и метод AreEqual
+            Assert.AreEqual("Привет..., Настя", res);
+        }
+    }
+}
---
+++ Tyuiu.SokolovaAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaAA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.SokolovaAA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вызов класса DataService и метода GetMessage
+            // из библиотеки Tyuiu.SokolovaAA.Sprint0.Task2.V0.Lib
+            Console.WriteLine(DataService.GetMessage("Настя"));
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
