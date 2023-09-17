e3f38ce76e0183fc77d37ea0f33d9d41e84912fc
Tyuiu.SokolovaAA@outlook.com <nas.sok@mail.ru>
2023-09-17 16:21:07 +0500

    Task7

---
+++ Tyuiu.SokolovaAA.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.SokolovaAA.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        //пример циклической структуры ( цикл с параметром) for
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0; i < resultArray.Length; i++)
+            {
+                resultArray[i] = numOne[i] + numTwo[i];
+            }
+            return resultArray;
+        }
+    }
+
+}
---
+++ Tyuiu.SokolovaAA.Sprint0.Task7.V0/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SokolovaAA.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.SokolovaAA.Sprint0.Task7.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт 0 | Выполнила: Соколова А.С. | АСОиУБ-23-3";
+            //длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт0                                                                 *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант#0                                                               *");
+            Console.WriteLine("* Выполнила: Соколова Анастасия Александровна | АСОиУб-23-3               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значение двух    *");
+            Console.WriteLine("* одинаковых массивов по длине                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            //место решения задания
+            int[] arrayNums1 = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("значение элементов массива №1");
+            for (int i = 0; i < arrayNums1.Length; i++)
+            {
+                Console.Write(arrayNums1[i] + ", ");
+            }
+
+            Console.WriteLine();
+            int[] arrayNums2 = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("значение элементов массива №2");
+            for (int i = 0; i < arrayNums2.Length; i++)
+            {
+                Console.Write(arrayNums2[i] + ", ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            if (arrayNums1.Length == arrayNums2.Length)
+            {
+                int[] resultArray = DataService.AdditionArrays(arrayNums1, arrayNums2);
+
+                Console.WriteLine("сумма элементов массива равна: ");
+                for (int i = 0; i < resultArray.Length; i++)
+                {
+                    Console.Write(resultArray[i] + ", ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Исходные мссивы имеют разное количество элементов");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
